fa3eb3e410e419997aa86774bb6e7977801b5253
gchernousov <seasidesuicide@yandex.ru>
2022-06-05 11:53:23 +0500

    initial commit

---
+++ dirty_main.py
@@ -0,0 +1,16 @@
+from application.salary import calculate_salary
+from application.people import get_employees
+
+from datetime import datetime
+
+
+if __name__ == "__main__":
+
+    print('Начало работы программы...')
+
+    print(f'Текущая дата: {datetime.now().date()}')
+
+    get_employees()
+    calculate_salary()
+
+    print('Конец работы программы...')
---
+++ main.py
@@ -0,0 +1,16 @@
+import application.salary as salary
+import application.people as people
+
+from datetime import datetime
+
+
+if __name__ == "__main__":
+
+    print('Начало работы программы...')
+
+    print(f'Текущая дата: {datetime.now().date()}')
+
+    people.get_employees()
+    salary.calculate_salary()
+
+    print('Конец работы программы...')
